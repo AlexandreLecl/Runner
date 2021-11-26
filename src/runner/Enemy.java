f3feb00e635c1b81a47063e3460b85c74c121837
alex_ <alexandre.leclercq@ensea.fr>
2021-11-26 22:26:10 +0100

    Enemy mobile

--- src/runner/Enemy.java
+++ src/runner/Enemy.java
@@ -5,9 +5,11 @@ import javafx.geometry.Rectangle2D;
 public class Enemy extends AnimatedThing{
     private long past=0;
     private double wait;
+    private double vx=5;
+    private double dt=1;
 
     public Enemy() {
-        super(500, 290, "run", 33, 1, 2, 30, 50, 48, 0, "file:Ghost_Sprites.png");
+        super(800, 290, "run", 33, 1, 2, 30, 50, 48, 0, "file:Ghost_Sprites.png");
     }
 
     public void update(long time){
@@ -21,5 +23,11 @@ public class Enemy extends AnimatedThing{
             past=time;
             this.sprite.setViewport(new Rectangle2D(290+index*offsetx,0,w,h));
         }
+        if(x<-10){
+            x=800;
+        }
+        x-= vx*dt;
+        sprite.setX(x);
+
     }
 }
